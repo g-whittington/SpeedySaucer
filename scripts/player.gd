a133304ec6ce302cbebc8b8f7335a18201ff4e17
g-whittington <105523254+g-whittington@users.noreply.github.com>
2024-09-29 00:35:52 -0400

    Added the _physics_process() function to the player

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,9 +1,16 @@
 extends RigidBody2D
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	# Only good for one time applied forces
+	# apply_force(Vector2(25,0))
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+	
+# physics process is ran at a fixed rate and not dependent on frame rate
+func _physics_process(delta: float) -> void:
+	apply_force(Vector2(25,0))
