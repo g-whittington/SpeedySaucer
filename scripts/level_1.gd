33533526a9374b98bc885c4c69192894b9817c1b
g-whittington <105523254+g-whittington@users.noreply.github.com>
2024-09-29 03:05:39 -0400

    Cleared unused functions in scripts

--- scripts/level_1.gd
+++ scripts/level_1.gd
@@ -1,10 +1,6 @@
 extends Node2D
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass
-
-# The player starts inside the maze. On exit, the maze will emit a signal
+ starts inside the maze. On exit, the maze will emit a signal
 # that we can handle here
 func _on_maze_1_body_exited(body: Node2D) -> void:
 	get_tree().reload_current_scene()
--- scripts/player.gd
+++ scripts/player.gd
@@ -2,16 +2,6 @@ extends RigidBody2D
 
 @export var move_force := 1000
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	# Only good for one time applied forces
-	# apply_force(Vector2(25,0))
-	pass
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-	
 # physics process is ran at a fixed rate and not dependent on frame rate
 func _physics_process(delta: float) -> void:
 	
