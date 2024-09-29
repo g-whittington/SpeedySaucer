2fe4fa04c0c757f9c57628bfd848fce01c70fa5f
g-whittington <105523254+g-whittington@users.noreply.github.com>
2024-09-29 01:01:54 -0400

    Created a basic movement system

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,5 +1,6 @@
 extends RigidBody2D
 
+@export var move_force := 500
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -13,4 +14,13 @@ func _process(delta: float) -> void:
 	
 # physics process is ran at a fixed rate and not dependent on frame rate
 func _physics_process(delta: float) -> void:
-	apply_force(Vector2(25,0))
+	
+	# Basic player movement checking if/when keys are pressed
+	if Input.is_action_pressed("up"):
+		apply_force(Vector2(0,-move_force))
+	if Input.is_action_pressed("down"):
+		apply_force(Vector2(0,move_force))
+	if Input.is_action_pressed("left"):
+		apply_force(Vector2(-move_force,0))
+	if Input.is_action_pressed("right"):
+		apply_force(Vector2(move_force,0))
