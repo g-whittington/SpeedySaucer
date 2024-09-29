408dd6bad6046702243b200917ddec9a7a1eb60f
g-whittington <105523254+g-whittington@users.noreply.github.com>
2024-09-29 03:22:44 -0400

    Made the game full screen and ESC quits game

--- scripts/level_1.gd
+++ scripts/level_1.gd
@@ -4,3 +4,7 @@ extends Node2D
 # that we can handle here
 func _on_maze_1_body_exited(body: Node2D) -> void:
 	get_tree().reload_current_scene()
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("exit"):
+		get_tree().quit()
