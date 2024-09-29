0887653d8a37b7584e0688e19e840911d3ab073d
g-whittington <105523254+g-whittington@users.noreply.github.com>
2024-09-29 03:08:41 -0400

    Anchored the background to full rect

--- scripts/level_1.gd
+++ scripts/level_1.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
- starts inside the maze. On exit, the maze will emit a signal
+# starts inside the maze. On exit, the maze will emit a signal
 # that we can handle here
 func _on_maze_1_body_exited(body: Node2D) -> void:
 	get_tree().reload_current_scene()
